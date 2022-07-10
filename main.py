c189e8a016f1efb58abe8f5ed918be74379d86b3
Wilson Gutierrez <wilsongutierrezl@gmail.com>
2022-07-10 16:46:02 -0500

    guardar los datos en archivo version 1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 
 def valorGasto():
   gasto = input("Por favor ingrese el valor: $")
@@ -7,8 +8,15 @@ def nombreProducto():
   producto = input("Por favor ingrese nombre del gasto: ").upper()
   return producto
 
+fila = [nombreProducto(), valorGasto()]
+
+nuevoArchivo = open('csv/income.csv', mode='w', encoding='utf-8-sig' )
+with nuevoArchivo:
+  writer = csv.writer(nuevoArchivo)
+  writer.writerow(fila)
+
+
 
 print("SISTEMA ADMINISTRADOR DE GASTOS PERSONALES")
 
-nombreProducto()
-valorGasto()
+
